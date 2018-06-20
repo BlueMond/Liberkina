be77118c58b3fa1ae601c8360854cf14d5adb4b6
BlueMond <thecoop416@gmail.com>
2018-06-20 18:13:09 -0400

    Rune Updates

--- src/me/bluemond/liberkina/Listeners/PlayerListener.java
+++ src/me/bluemond/liberkina/Listeners/PlayerListener.java
@@ -1,5 +1,7 @@
 package me.bluemond.liberkina.Listeners;
 
+import me.bluemond.liberkina.Runes.Rune;
+import me.bluemond.liberkina.Runes.RuneRegistry;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
@@ -9,18 +11,28 @@ import org.bukkit.event.player.PlayerJoinEvent;
 
 import java.util.ArrayList;
 
+
 public class PlayerListener implements Listener {
 
-    public PlayerListener(){
+    ArrayList<Rune> runes;
 
+    public PlayerListener(){
+        runes = RuneRegistry.getRegistry();
     }
 
     @EventHandler
     public void onBreak(BlockBreakEvent event){
         Block block = event.getBlock();
+        ArrayList<Rune> eRunes = new ArrayList<Rune>();
 
-        if(){
+        for(Rune rune : runes){
+            if(rune.getCore().equals(block.getType())){
 
+                eRunes.add(rune);
+
+            }
         }
+
+
     }
 }
--- src/me/bluemond/liberkina/Runes/Rune.java
+++ src/me/bluemond/liberkina/Runes/Rune.java
@@ -1,5 +1,28 @@
 package me.bluemond.liberkina.Runes;
 
-public class Rune {
+import org.bukkit.Location;
+import org.bukkit.Material;
 
+
+public class Rune {
+    Material[][] grid;
+    Material core;
+
+    public Rune(Material[][] grid){
+        this.grid = grid;
+        findCore();
+    }
+
+    public void activate(Location loc){
+
+    }
+
+    private void findCore(){
+        int mid = grid.length/2 - 1;
+        core = grid[mid][mid];
+    }
+
+    public Material getCore(){
+        return core;
+    }
 }
